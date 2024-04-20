cef13e75ead95830aaa425b2582d9815123efcfc
Rubber01 <biasotp@gmail.com>
2024-04-20 23:31:23 +0200

    nav mesh fix

--- Assets/_Scripts/RaidManager.cs
+++ Assets/_Scripts/RaidManager.cs
@@ -7,6 +7,7 @@ public class RaidManager : MonoBehaviour
     [SerializeField] private List<GameObject> previousCollidedObjects = new List<GameObject>();
     [SerializeField] private bool countdownStarted = false;
     [SerializeField] private bool countupStarted = false;
+    [SerializeField] private int alliesRequired;
     private bool raided = false;
     [SerializeField] private float delay = 2;
     [SerializeField] private float countdownTime = 10f; 
@@ -30,17 +31,18 @@ public class RaidManager : MonoBehaviour
 
     private void OnCollisionStay(Collision collision)
     {
+        Debug.Log("allies hitting " + previousCollidedObjects.Count);
         //Debug.LogError("Collision Stay");
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player") ||
-            collision.gameObject.layer == LayerMask.NameToLayer("Ally"))
+            collision.gameObject.layer == LayerMask.NameToLayer("Ally") )
         {
             if (!previousCollidedObjects.Contains(collision.gameObject))
             {
                 previousCollidedObjects.Add(collision.gameObject);
 
-                if (!countdownStarted)
+                if (!countdownStarted && previousCollidedObjects.Count - 1 >= alliesRequired)
                 {
-                    StartCoroutine(CountUp());
+                    StartCoroutine(CountDown());
                 }
             }
         }
@@ -64,40 +66,43 @@ public class RaidManager : MonoBehaviour
             
         }
         if (previousCollidedObjects.Count == 0)
-            StartCoroutine(CountUp());
+            StartCoroutine(CountUp(delay));
     }
-  
-    IEnumerator CountUp()
-    {
-        if (previousCollidedObjects.Count == 0 && countupStarted == false && countdownTime < originalCountdownTime)
-        {
 
-            while (countdownTime < originalCountdownTime)
-            {
-                countdownStarted = false;
-                countupStarted = true;
-                Debug.Log("Countup: " + countdownTime.ToString("F1") + " seconds" + " numero di oggetti " + previousCollidedObjects.Count);
-                countdownTime++;
-                healthBar.UpdateHealthBar(originalCountdownTime, countdownTime);
-                yield return new WaitForSeconds(1f);
-            }
-        }else if (previousCollidedObjects.Count != 0 && countdownStarted==false && countdownTime!=0)
+    IEnumerator CountDown()
+    {
+        while (countdownTime > 0 && previousCollidedObjects.Count > 0)
         {
-            while (countdownTime > 0 && previousCollidedObjects.Count > 0)
-            {
-                countupStarted = false;
-                countdownStarted = true;
-                Debug.Log("Countdown: " + countdownTime.ToString("F1") + " seconds" + " numero di oggetti " + previousCollidedObjects.Count);
-                countdownTime -= previousCollidedObjects.Count;
-                healthBar.UpdateHealthBar(originalCountdownTime, countdownTime);
-                yield return new WaitForSeconds(1f);
-            }
+            countupStarted = false;
+            countdownStarted = true;
+            Debug.Log("Countdown: " + countdownTime.ToString("F1") + " seconds" + " numero di oggetti " + previousCollidedObjects.Count);
+            countdownTime -= previousCollidedObjects.Count;
+            healthBar.UpdateHealthBar(originalCountdownTime, countdownTime);
+            yield return new WaitForSeconds(1f);
+        }
+
+    }
+    IEnumerator CountUp(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        while (countdownTime < originalCountdownTime)
+        {
+            countdownStarted = false;
+            countupStarted = true;
+            Debug.Log("Countup: " + countdownTime.ToString("F1") + " seconds" + " numero di oggetti " + previousCollidedObjects.Count);
+            countdownTime++;
+            healthBar.UpdateHealthBar(originalCountdownTime, countdownTime);
+            yield return new WaitForSeconds(1f);
         }
     }
     private void Update()
     {
-        
-         
+        if (previousCollidedObjects.Count == 0 && countupStarted == false && countdownTime < originalCountdownTime)
+        {
+
+            StartCoroutine(CountUp(delay));
+        }
+
         if (countdownTime == 0 && raided == false)
         {
             raided = true;
