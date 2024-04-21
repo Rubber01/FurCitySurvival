f09cb52b60db02ad32a4f2eca4e36856507ce489
Rubber01 <biasotp@gmail.com>
2024-04-21 15:28:01 +0200

    Buba

--- Assets/_Scripts/AnimatorController.cs
+++ Assets/_Scripts/AnimatorController.cs
@@ -15,33 +15,63 @@ public class AnimatorController : MonoBehaviour
     {
         Debug.Log("Is moving :" + _animator.GetBool("IsMoving"));
         Debug.Log("Is attacking :" + _animator.GetBool("IsAttacking"));
+        Debug.Log("Is dying :" + _animator.GetBool("IsDying"));
+        Debug.Log("Is happy :" + _animator.GetBool("IsAttacking"));
     }
     public void PlayIdle()
     {
         _animator.SetBool("IsMoving", false);
+        _animator.SetBool("IsAttacking", false);
+        _animator.SetBool("IsDying", false);
+        _animator.SetBool("IsHappy", false);
+
         //_animator.Play("Idle");
     }
 
     public void PlayRun()
     {
         _animator.SetBool("IsMoving", true);
+        _animator.SetBool("IsAttacking", false);
+        _animator.SetBool("IsDying", false);
+        _animator.SetBool("IsHappy", false);
         //_animator.Play("Run");
     }
     public void PlayHit()
     {
         _animator.SetBool("IsAttacking", true);
-/*
-        if (Random.Range(0, 1) == 0)
-        {
-            _animator.Play("LowHit");
-        }
-        else
-        {
-            _animator.Play("HighHit");
-        }*/
+        _animator.SetBool("IsMoving", false);
+        _animator.SetBool("IsDying", false);
+        _animator.SetBool("IsHappy", false);
+        /*
+                if (Random.Range(0, 1) == 0)
+                {
+                    _animator.Play("LowHit");
+                }
+                else
+                {
+                    _animator.Play("HighHit");
+                }*/
     }
     public void PlayStopHit()
     {
+        _animator.SetBool("IsAttacking", false);
+        _animator.SetBool("IsDying", false);
+        _animator.SetBool("IsHappy", false);
+
+    }
+    public void PlayIsHappy()
+    {
+        _animator.SetBool("IsHappy", true);
+        _animator.SetBool("IsMoving", false);
+        _animator.SetBool("IsDying", false);
+        _animator.SetBool("IsAttacking", false);
+
+    }
+    public void PlayIsDying()
+    {
+        _animator.SetBool("IsHappy", false);
+        _animator.SetBool("IsMoving", false);
+        _animator.SetBool("IsDying", true);
         _animator.SetBool("IsAttacking", false);
 
     }
