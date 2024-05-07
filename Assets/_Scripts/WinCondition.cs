e133bbe5242c886ef83834fe7a54b3003040ff91
Liuk87 <40986159+Liuk87@users.noreply.github.com>
2024-05-08 00:30:42 +0200

    fix

--- Assets/_Scripts/WinCondition.cs
+++ Assets/_Scripts/WinCondition.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class WinCondition : MonoSingleton<WinCondition>
 {
